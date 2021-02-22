c01d1f2d7fc7030ea95a9751812f112ae90b147e
Dnoel26 <darnellnoel.webdev@gmail.com>
2021-02-22 02:19:54 -0400

    commit 22 - major stylistic changes to about section, minor changes other sections

--- js/app.js
+++ js/app.js
@@ -27,12 +27,12 @@ const App = {
                 opacity: [1,0],
                 easing: "easeInExpo",
                 duration: 600,
-                delay: (el, i) => 12000 + 30 * i,
+                delay: (el, i) => 6000 + 30 * i,
             });
         
         // Wrap every letter in a span
 
-        const textWrapperML9 = document.querySelector('.ml9 .letters');
+        /*const textWrapperML9 = document.querySelector('.ml9 .letters');
         textWrapperML9.innerHTML = textWrapperML9.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
         anime.timeline({loop: true})
@@ -48,7 +48,7 @@ const App = {
                 duration: 1500,
                 easing: "easeOutExpo",
                 delay: 11000,
-            });
+            });*/
 
         /*** Bootstrap Scrollspy ***/
 
@@ -73,9 +73,9 @@ const App = {
         const skill_ratings_advanced = document.querySelectorAll(".skill-rating-advanced");
         const skill_ratings_expert = document.querySelectorAll(".skill-rating-expert");
 
-        const body = document.querySelector("main");
+        const body = document.querySelector("body");
 
-        const about_section = document.querySelector(".about-section .container");
+        const about_summary_wrapper = document.querySelector(".about-section .summary-wrapper");
         const my_age = document.querySelector("[data-age='my-age']");
         const summary_containers = document.querySelectorAll(".summary-container");
         const summary_btn = document.querySelector("[data-id='summary-btn']");
@@ -191,7 +191,7 @@ const App = {
 
                 // Resize header when scrolling - adds artificial height to compensate for reduction in header height
 
-                if(document.documentElement.scrollTop > 1 || window.pageYOffset > 1) {
+                if(document.documentElement.scrollTop > 2 || window.pageYOffset > 2) {
 
                     //console.log(window.pageYOffset);
                     body.classList.remove("will-change-height");
@@ -251,7 +251,7 @@ const App = {
 
             // Automatically adjust my age in bio based on date
 
-            function calculate_age(dob) { 
+            function calculate_age() { 
                 const diff_ms = Date.now() - new Date("26 March 1990");
                 const age_dt = new Date(diff_ms); 
               
@@ -259,7 +259,7 @@ const App = {
             }
 
             //console.log(calculate_age(new Date(1982, 20, 4)));
-            my_age.innerHTML = calculate_age("06/24/2008");
+            my_age.innerHTML = calculate_age();
 
             // Adds a fade in and out effect when clicking the button in my bio
 
@@ -269,7 +269,7 @@ const App = {
 
                 if(summary_containers[1].classList.contains("d-none")) {
 
-                    about_section.classList.add("opacity-0");
+                    about_summary_wrapper.classList.add("opacity-0");
 
                     setTimeout(() => {
                         
@@ -280,22 +280,22 @@ const App = {
 
                     setTimeout(() => {
 
-                        about_section.classList.remove("opacity-0");
+                        about_summary_wrapper.classList.remove("opacity-0");
                     }, 310);
                 } else if(summary_containers[0].classList.contains("d-none")) {
 
-                    about_section.classList.add("opacity-0");
+                    about_summary_wrapper.classList.add("opacity-0");
 
                     setTimeout(() => {
                         
                         summary_containers[1].classList.add("d-none", "opacity-0");
                         summary_containers[0].classList.remove("d-none", "opacity-0");
-                        summary_btn.innerHTML = "Learn More...";
+                        summary_btn.innerHTML = "Learn More";
                     }, 110);
 
                     setTimeout(() => {
 
-                        about_section.classList.remove("opacity-0");
+                        about_summary_wrapper.classList.remove("opacity-0");
                     }, 310);
                 };
             });
@@ -325,19 +325,19 @@ const App = {
                 classification = "";
 
                 filled_icon = ` 
-                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
                                     <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                                 </svg>
                              `;
 
                 half_icon = `                                       
-                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-star-half" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-star-half" viewBox="0 0 16 16">
                                     <path d="M5.354 5.119L7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.55.55 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.519.519 0 0 1-.146.05c-.341.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.171-.403.59.59 0 0 1 .084-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027c.08 0 .16.018.232.056l3.686 1.894-.694-3.957a.564.564 0 0 1 .163-.505l2.906-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.002 2.223 8 2.226v9.8z"/>
                                 </svg>
                            `;
 
                 empty_icon = ` 
-                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
                                     <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                                 </svg>
                             `;
@@ -452,7 +452,7 @@ const App = {
 
                         function error() {
 
-                            status.innerHTML = 'Oops, sorry! There was a problem with the form submission. Ensure to check "I am not a robot" and try again or use an alternate method to contact me.';
+                            status.innerHTML = 'Oops, sorry! There was a problem with the form submission. Ensure to check "I\'m not a robot" and try again or use an alternative method to contact me.';
                         };
 
                         if (!form.checkValidity()) {
